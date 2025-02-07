459d002053fe87314d8407c7e277cad3adb67e5b
Egbert <eddeet2001@gmail.com>
2025-02-07 10:21:17 +0200

    Some nice refactoring to clean up the code

--- FinalTechTest/Services/LocationService.cs
+++ FinalTechTest/Services/LocationService.cs
@@ -4,99 +4,66 @@
 
     public class LocationService
     {
-        public event Action? OnLocationChanged;
-        public event Action? OnLocationChangedFromInput;
-        public event Action? OnAddMarkerRequested; // New event for triggering marker addition
+        public event Action? OnLocationChangedfromMapAction; //Location 
+        public event Action? OnLocationChangedFromInputAction;
+        public event Action? OnAddMarkerRequestedAction; // New event for triggering marker addition
         public event Action? UpdateMarkerFromInputAction; // New event for triggering marker addition
         public event Action? UpdateMarkerFromMapAction; // New event for triggering marker moving
-    
+
         public Func<Task>? LockMarkerAndAddPopupAction;
         public event Action? RemoveMarkerAction; // New event for triggering marker removal
-        private double latitude;
-        private double longitude;
-        private string name;
-        private string? _newName;
-        private double? _newLatitude = 0;
-        private double? _newLongitude = 0;
-        
+        public double Latitude { get; private set; }
+        public double Longitude { get; private set; }
+        public string Name { get; private set; }
+
+
+        public string MarkerName { get; set; }
+        public double MarkerLatitude { get; private set; }
+        public double MarkerLongitude { get; private set; }
 
 
 
-        public void SetLocation(double Lat, double Lng, string Name, string locName)
+
+        public void SetLocation(double lat, double lng, string name, string source)
         {
-            latitude = Lat;
-            longitude = Lng;
-            name = Name;
-            if(locName == "map")
-                OnLocationChanged?.Invoke(); // Notify listeners on Blazor side that location has changed
-            if(locName == "input")
-                OnLocationChangedFromInput?.Invoke(); // Notify listeners on Map side that location has changed
+            Latitude = lat;
+            Longitude = lng;
+            Name = name;
+            if (source == "map")
+                OnLocationChangedFromInputAction?.Invoke(); // Notify listeners on Blazor side that location has changed
+            if (source == "input")
+                OnLocationChangedFromInputAction?.Invoke(); // Notify listeners on Map side that location has changed
         }
-
-        public double GetLatitude()
-        {
-            return latitude;
-        }
-
-        public double GetLongitude()
-        {
-            return longitude;
-        }
-
-        public string GetName()
-        {
-            return name;
-        }
-
 
         public void RequestAddMarker()
         {
-            OnAddMarkerRequested?.Invoke(); // Notify listeners that a new marker should be added
+            OnAddMarkerRequestedAction?.Invoke(); // Notify listeners that a new marker should be added
         }
 
-        #region Get and Set for New location
         public void UpdateMarkerfromInput(double newLatitude, double newLongitude)
         {
-            _newLatitude = newLatitude;
-            _newLongitude = newLongitude;
+            MarkerLatitude = newLatitude;
+            MarkerLongitude = newLongitude;
             UpdateMarkerFromInputAction?.Invoke();
         }
 
         public void UpdateMarkerfromMap(double newLatitude, double newLongitude)
         {
-            _newLatitude = newLatitude;
-            _newLongitude = newLongitude;
+            MarkerLatitude = newLatitude;
+            MarkerLongitude = newLongitude;
             UpdateMarkerFromMapAction?.Invoke();
         }
 
-        
-        public void SetNewName(string newName)
+        public void SetMarkerName(string name)
         {
-            _newName = newName;
+            MarkerName = name;
         }
-
-        public string? GetNewName()
-        {
-            return _newName;
-        }
-
-        public double? GetNewLatitude()
-        {
-            return _newLatitude;
-        }
-
-
-        public double? GetNewLongitude()
-        {
-            return _newLongitude;
-        }
-        #endregion
 
         public async Task LockMarkerAndAddPopup()
         {
             if (LockMarkerAndAddPopupAction != null)
             {
-                await LockMarkerAndAddPopupAction.Invoke(); // ✅ Ensures execution is awaited
+                await LockMarkerAndAddPopupAction.Invoke(); //Ensures execution is awaited
             }
         }
 
